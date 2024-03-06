5f7c10b22ac7813919547234ffb4985a64b2831a
Muhammad Rehman Zulfiquar <mrzulfiquar@gmail.com>
2024-03-06 01:08:08 +0000

    Use swagger for published api

--- Program.cs
+++ Program.cs
@@ -16,10 +16,9 @@ namespace AuctionBackend
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
 
             // Add services to the container.
             builder.Services.AddControllers();
@@ -61,16 +60,12 @@ namespace AuctionBackend
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
-
-
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            
 
             app.MapControllers();
 
@@ -80,19 +75,20 @@ namespace AuctionBackend
 
             app.MapControllers();
 
-
             using (var scope = app.Services.CreateScope())
             {
                 var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<AuctionContext>();
 
                 try
                 {
-                    Seeder.InitializeAsync(services).Wait();
+                    context.Database.Migrate();
+                    await Seeder.InitializeAsync(services);
                 }
                 catch (Exception ex)
                 {
                     var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred seeding the DB.");
+                    logger.LogError(ex, "An error occurred seeding the Database.");
                 }
             }
 
